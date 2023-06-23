b59cedd8beeef74741ee592e163c822d296984ae
hugonogo <gohupelief@gmail.com>
2023-06-22 22:48:26 -0300

    updateCommit

---
+++ app/src/main/java/com/example/appform/Formulario2.java
@@ -0,0 +1,14 @@
+package com.example.appform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formulario2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario2);
+    }
+}
