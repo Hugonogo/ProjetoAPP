9bdd5f005a0282bac77656c0923a973af6256f72
hugonogo <gohupelief@gmail.com>
2023-07-27 18:24:34 -0300

    Atualização do contador de pasos, adição de uma barra de progresso e um reset

--- app/src/main/java/com/example/appform/StepCount.java
+++ app/src/main/java/com/example/appform/StepCount.java
@@ -1,20 +1,19 @@
 package com.example.appform;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.pm.PackageManager;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -22,9 +21,11 @@ public class StepCount extends AppCompatActivity implements SensorEventListener 
     private SensorManager sensorManager;
     private Sensor stepCounterSensor;
     private int stepCount = 0;
+    private int previewCount = 0;
     private boolean isSensorPresent;
     Context context;
     private TextView countTextView;
+    private ProgressBar circleBar;
 
 
 
@@ -34,10 +35,11 @@ public class StepCount extends AppCompatActivity implements SensorEventListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_count);
         context = this;
-
+        circleBar = findViewById(R.id.progres_steps);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         countTextView = findViewById(R.id.Count_TextView);
 
+        resetStep();
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
@@ -58,16 +60,20 @@ public class StepCount extends AppCompatActivity implements SensorEventListener 
     protected void onResume() {
         super.onResume();
         if (isSensorPresent && sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
-            sensorManager.registerListener(this, stepCounterSensor, sensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(this, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
 
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (sensorEvent.sensor == stepCounterSensor) {
             stepCount = (int) sensorEvent.values[0];
-            countTextView.setText(String.valueOf(stepCount));
+            int steptaken = stepCount - previewCount;
+            countTextView.setText("Passos: " + steptaken);
+
+            circleBar.setProgress(steptaken);
         }
     }
 
@@ -88,4 +94,21 @@ public class StepCount extends AppCompatActivity implements SensorEventListener 
 
 
     }
+
+    @SuppressLint("SetTextI18n")
+    public void resetStep(){
+
+        countTextView.setOnClickListener(viewClick -> Snackbar.make(viewClick, "Segure Para Resetar o Contador", Snackbar.LENGTH_SHORT).show());
+
+        countTextView.setOnLongClickListener(viewLongClick ->{
+
+            previewCount = stepCount;
+            countTextView.setText("Passos: 0");
+            circleBar.setProgress(0);
+            return true;
+        });
+
+    }
+
+
 }
