f4670446c40178b19ce9123c509ecae4f728b5cb
gusta <gustavo024dias@gmail.com>
2023-08-01 22:19:41 -0300

    sistema de login

--- app/src/main/java/com/example/appform/MainActivity.java
+++ app/src/main/java/com/example/appform/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.appform;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
@@ -9,6 +10,10 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.appform.databinding.ActivityFormularioBinding;
+import com.example.appform.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,11 +25,13 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogin;
     
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private ActivityMainBinding vb ;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        vb = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(vb.getRoot());
         btnCad = findViewById(R.id.btn_cad);
         btnLogin = findViewById(R.id.login_btn);
 
@@ -40,9 +47,23 @@ public class MainActivity extends AppCompatActivity {
         }
         
         btnLogin.setOnClickListener(vLogin ->{
-            startActivity(
-                    new Intent(getApplicationContext(), StepCount.class)
-            );
+            String email, senha;
+            email = vb.edtEmail.getText().toString().trim();
+            senha = vb.Password.getText().toString().trim();
+
+            if ( email != null && senha != null){
+                auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(task -> {
+                    if ( task.isSuccessful() ){
+                        startActivity(
+                                new Intent(getApplicationContext(), StepCount.class)
+                        );
+                    }else{
+                        Toast.makeText(MainActivity.this, "Credenciais erradas.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+
         });
 
 
